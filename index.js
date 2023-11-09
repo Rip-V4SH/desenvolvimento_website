939b6fe2d0d6c9ad0542dab9c685af6b1d1a0192
EA - Igor Fernandes Almeida <igor_fernandes@educadventista.org>
2023-11-09 15:48:24 -0300

    pagina de cadastro feita

--- index.js
+++ index.js
@@ -1,3 +1,4 @@
+const e = require("express")
 const express = require("express")
 
 const path = require("path")
@@ -5,6 +6,28 @@ const path = require("path")
 const app = express()
 
 const caminhoBase = path.join(__dirname, "templates")
+
+app.use(express.urlencoded({
+    extended: true
+}))
+
+app.use(express.json())
+
+app.post('/cadastrar/salvar', (requisicao, resposta) => {
+    // console.log(requisicao.body)
+    const nome = requisicao.body.nome
+    const email = requisicao.body.email
+    const senha = requisicao.body.senha
+
+    console.log(`email : ${email}`)
+    console.log(`nome : ${nome}`)
+    console.log(`senha : ${senha}`)
+
+})
+
+app.get('/cadastrar', (requisicao, resposta) => {
+    resposta.sendFile(`${caminhoBase}/cadastro.html`)
+})
 
 app.get('/usuarios/:id', (requisicao, resposta) => {
     const id = requisicao.params.id
